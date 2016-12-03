ddfc236cd55bf902eeec266f61b70165a4638aac
TanakaHiroki0518 <tatiu.0518@gmail.com>
2016-12-03 20:36:31 +0900

    answer author tanaka_hiroki

--- test/src/fizzbuzz/FizzBuzz.java
+++ test/src/fizzbuzz/FizzBuzz.java
@@ -9,10 +9,23 @@ package fizzbuzz;
  * ・それ以外の場合：数値を出力
  */
 public class FizzBuzz {
+	private boolean isFizz = false;
+	private boolean isBuzz = false;
+
 	/**
 	 * fizzbuzz実行関数
 	 * @param fizbuzMax (int)fizzbuzz実行時の最大値
 	 */
 	public void exec (int fizbuzMax) {
+		for (int i = 1; i <= fizbuzMax; i++) {
+			isFizz = (i % 3 == 0);
+			isBuzz = (i % 5 == 0);
+
+			if (isFizz || isBuzz) {
+				System.out.print((isFizz ? "fizz" : "") + (isBuzz ? "buzz" : "") + "\n");
+			} else {
+				System.out.print(String.valueOf(i) + "\n");
+			}
+		}
 	}
 }
